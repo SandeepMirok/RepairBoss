074f022e92eabba721e61fade9894395be3f9007
Andre Lemos <lemosandrecosta@gmail.com>
2018-08-13 14:29:12 -0400

    Fixed erreos

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import "./Home.css";
 class Home extends Component {
   render() {
     return (
-      <div class="container">
+      <div className="container">
         <header className="blog-header py-3">
           <div className="row flex-nowrap justify-content-between align-items-center">
             <div className="col-4 pt-1">
@@ -18,23 +18,6 @@ class Home extends Component {
               </a>
             </div>
             <div className="col-4 d-flex justify-content-end align-items-center">
-              <a className="text-muted" href="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  className="mx-3"
-                >
-                  <circle cx="10.5" cy="10.5" r="7.5" />
-                  <line x1="21" y1="21" x2="15.8" y2="15.8" />
-                </svg>
-              </a>
               <a className="btn btn-sm btn-outline-secondary" href="">
                 Sign up
               </a>
