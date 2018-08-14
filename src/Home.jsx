772c72d03e886e70df03af614b8c09c505218f85
AAmber_Fan <yuxuan.fan11@gmail.com>
2018-08-14 11:51:51 -0400

    add header

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,13 @@
 import React, { Component } from "react";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Header from "./Header";
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
-        <header className="blog-header py-3">
-          <div className="row flex-nowrap justify-content-between align-items-center">
-            <div className="col-4 pt-1">
-              <a className="text-muted" href="">
-                Subscribe
-              </a>
-            </div>
-            <div className="col-4 text-center">
-              <a className="blog-header-logo text-dark" href="">
-                AutoRepair
-              </a>
-            </div>
-            <div className="col-4 d-flex justify-content-end align-items-center">
-              <a className="btn btn-sm btn-outline-secondary" href="">
-                Login
-              </a>
-            </div>
-          </div>
-        </header>
+       <Header/>
         <div className="nav-scroller py-1 mb-2">
           <nav className="nav ">
             <a className="p-2 text-muted flex-auto " href="">
