ec481ff49ecf15352591939495e95a90359506dd
AAmber_Fan <yuxuan.fan11@gmail.com>
2018-08-14 12:28:24 -0400

    add nav and content folder

---
+++ src/Nav.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+class Nav extends Component{
+    render(){
+        return (
+            <div className="nav-scroller py-1 mb-2">
+                <nav className="nav ">
+                    <a className="p-2 text-muted flex-auto " href="">
+                        Home
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        Service & Repair
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        Repair Prices
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        Diagnostic
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        Reviews
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        My Car
+                    </a>
+                    <a className="p-2 text-muted flex-auto" href="">
+                        Contact
+                    </a>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Nav;
