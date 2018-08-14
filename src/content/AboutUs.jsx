f06a24577b6550cfe102e50d6f99e257b191410c
Andre Lemos <lemosandrecosta@gmail.com>
2018-08-14 14:37:17 -0400

    Photo Add

--- src/content/AboutUs.jsx
+++ src/content/AboutUs.jsx
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../style/Home.css';
-import '../style/content.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "../style/Home.css";
+import "../style/content.css";
+import Ace from "../img/Ace.jpg";
 
+class AboutUs extends Component {
+  render() {
+    return (
+      <div className="about" id="about">
+        <div className="container">
+          <h3 className="title">About Us</h3>
 
-class AboutUs extends Component{
-    render(){
-        return(<div className="about" id="about">
-            <div className="container">
-                <h3 className="title">About Us</h3>
-
-                <div className=" about-right wow fadeInRight animated" data-wow-delay=".5s">
-                     {/* style={{visibility: visible, -webkit-animation-delay: .5s;}}>*/}
-                    <h4>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                        classical Latin established fact that a reader will be distracted Lorem Ipsum when looking at
-                        its layout.</h4>
-                    <p>Lorem Ipsum was popularised in the 1960s with the release of Letraset sheet content of a page
-                        when looking at its layout The point of using Lorem Ipsum is that it has a more-or-less normal
-                        distribution is that it has a more of letters, as opposed to using 'Content here, content here',
-                        making it look like readable English. Many desktop publishing packages and web page editors now
-                        use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many
-                        web sites still in their infancy. </p>
-                </div>
-                <div className="clearfix"></div>
-            </div>
-        </div>)
-
-    }
+          <div
+            className=" about-right wow fadeInRight animated"
+            data-wow-delay=".5s"
+          >
+            {/* style={{visibility: visible, -webkit-animation-delay: .5s;}}>*/}
+            <h4>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin established fact that a
+              reader will be distracted Lorem Ipsum when looking at its layout.
+            </h4>
+            <p>
+              Lorem Ipsum was popularised in the 1960s with the release of
+              Letraset sheet content of a page when looking at its layout The
+              point of using Lorem Ipsum is that it has a more-or-less normal
+              distribution is that it has a more of letters, as opposed to using
+              'Content here, content here', making it look like readable
+              English. Many desktop publishing packages and web page editors now
+              use Lorem Ipsum as their default model text, and a search for
+              'lorem ipsum' will uncover many web sites still in their infancy.{" "}
+            </p>
+          </div>
+          <div className="clearfix">
+            <img src={Ace} class="img-thumbnail" alt="Cinque Terre" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default AboutUs;
