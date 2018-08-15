079dd78f7939dc04af89760540f297c8ff98172b
AAmber_Fan <yuxuan.fan11@gmail.com>
2018-08-15 13:25:33 -0400

    add ServicePart and Add some animation

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,13 @@ import Header2 from "./Header2";
 import Footer from "./Footer";
 import Nav from "./Nav";
 import AboutUs from "./content/AboutUs";
+import WOW from 'wowjs';
+import ServicePart from "./content/Service";
 
 class Home extends Component {
+    componentDidMount(){
+        new WOW.WOW().init();
+    }
   render() {
     return (
       <div className="container">
@@ -15,6 +20,7 @@ class Home extends Component {
         <Nav />
         <Header2 />
         <AboutUs />
+          <ServicePart/>
         <div>
           <Footer />
         </div>
