3c6d90001e820d29623a3752877ec14588896352
SandeepMirok <35110835+SandeepMirok@users.noreply.github.com>
2018-08-29 10:05:42 -0400

    Testing

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,8 @@ import "bootstrap/dist/css/bootstrap.css";
 
 class Header extends Component {
   render() {
-    return (
+    return {
+      /*
       <div className="App">
         <header className="blog-header py-3">
           <div className="row flex-nowrap justify-content-between align-items-center">
@@ -26,7 +27,8 @@ class Header extends Component {
           </div>
         </header>
       </div>
-    );
+      */
+    };
   }
 }
 
